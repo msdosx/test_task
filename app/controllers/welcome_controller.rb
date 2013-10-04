adc0675e72520b2253baf2144bdd570bb762f5ac
msdos <1msdos1@gmail.com>
2013-10-04 14:37:30 +0400

    error redirect and error page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    redirect_to (errors_show_path) if !params[:error].nil?
+    #error_code = params[:error_description] if !params[:error_description].nil?
+    #redirect_to errors_show_path, :error_description => "foo"  if !params[:error].nil?
+    redirect_to :controller => :errors, :action => :show, :error_description => params[:error_description]  if !params[:error].nil?
   end
 end
